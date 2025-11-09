70ed51e9f796b0ef4dff2b96fbb3049022fe8a8d
LF2Zetrei <fernandesl@cy-tech.fr>
2025-11-09 16:09:02 +0100

    add JWT utils -> generate token, get username by token and validate token And add password encoder method

--- src/main/java/org/example/authapi/security/JWTUtils.java
+++ src/main/java/org/example/authapi/security/JWTUtils.java
@@ -1,4 +1,52 @@
 package org.example.authapi.security;
 
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.example.authapi.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JWTUtils {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpiration;
+
+    public String generateJwtToken(String username) {
+        Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + jwtExpiration))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(jwtSecret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(jwtSecret.getBytes())
+                    .build()
+                    .parseClaimsJws(token);
+                return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
 }
