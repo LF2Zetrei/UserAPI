2f9b09647c29fe344b2e8a115157758abed0c1fe
LF2Zetrei <fernandesl@cy-tech.fr>
2025-11-09 17:19:35 +0100

    add Logout requestDto

--- src/main/java/org/example/authapi/dto/LogoutRequest.java
+++ src/main/java/org/example/authapi/dto/LogoutRequest.java
@@ -1,4 +1,11 @@
 package org.example.authapi.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LogoutRequest {
+    private String jwt;
+    private String refreshToken;
 }
